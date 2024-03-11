2b2a0d9de1aaf65e332021c316870dd51372b2a8
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-03-11 22:34:17 +0400

    add products and fix tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
+import { sampleProducts } from "./data";
 function App() {
   return (
-    <div>
-      <header>AMAZONA</header>
-      <main></main>
-      <footer>ALL rights reserved</footer>
+    <div className="">
+      <header className=" flex justify-center mb-10">
+        <h1 className=" text-3xl">AMAZONA</h1>
+      </header>
+      <main className=" flex justify-center">
+        <ul className="">
+          {sampleProducts.map((product) => (
+            <li
+              key={product.slug}
+              className=" mb-10 flex flex-col justify-center items-center gap-2"
+            >
+              <img className="" src={product.image} alt={product.name} />
+              <h2 className="">{product.name}</h2>
+              <p className="">{product.price}$</p>
+            </li>
+          ))}
+        </ul>
+      </main>
+      <footer className="">ALL rights reserved</footer>
     </div>
   );
 }
