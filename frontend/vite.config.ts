56d8ee403f388c36d5d2597f5ad928dcfd642eef
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-03-26 18:10:49 +0400

    debugging for make dist

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,12 @@
 // https://vitejs.dev/config/
-export default {
+import { defineConfig } from "vite";
+export default defineConfig({
   server: {
     proxy: {
       "/api": "http://localhost:4000", // Replace YOUR_BACKEND_PORT with your actual backend server port
     },
   },
-};
+  build: {
+    outDir: "dist",
+  },
+});
